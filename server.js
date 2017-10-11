44e128c5443a237a6b23b5a936a44884477fa260
Hugo Alberto Ramírez <hugo@empowerdata.mx>
2017-10-11 18:07:52 -0500

    first commit/server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let https = require ('https');
+
+let accessKey = 'enter key here';
+let uri = 'westus.api.cognitive.microsoft.com';
+let path = '/text/analytics/v2.0/sentiment';
+
+let response_handler = function (response) {
+    let body = '';
+    response.on ('data', function (d) {
+        body += d;
+    });
+    response.on ('end', function () {
+        let body_ = JSON.parse (body);
+        let body__ = JSON.stringify (body_, null, '  ');
+        console.log (body__);
+    });
+    response.on ('error', function (e) {
+        console.log ('Error: ' + e.message);
+    });
+};
+
+let get_sentiments = function (documents) {
+    let body = JSON.stringify (documents);
+
+    let request_params = {
+        method : 'POST',
+        hostname : uri,
+        path : path,
+        headers : {
+            'Ocp-Apim-Subscription-Key' : accessKey,
+        }
+    };
+
+    let req = https.request (request_params, response_handler);
+    req.write (body);
+    req.end ();
+}
+
+let documents = { 'documents': [
+    { 'id': '1', 'language': 'en', 'text': 'I really enjoy the new XBox One S. It has a clean look, it has 4K/HDR resolution and it is affordable.' },
+    { 'id': '2', 'language': 'es', 'text': 'Este ha sido un dia terrible, llegué tarde al trabajo debido a un accidente automobilistico.' },
+]};
+
+get_sentiments (documents);
